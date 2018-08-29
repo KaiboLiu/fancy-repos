4e45f532a2309ec8ced5b55b5b59e31d0f24e996
Liu Kaibo <liukaibo@Kaibo-MacBook-Pro.local>
2018-08-29 13:16:17 -0700

    shanghai data collected

---
+++ spider_house/clean.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sys
+
+fileName = 'rent_lianjia_bj_0.txt' if len(sys.argv) == 1 else sys.argv[1]
+f = open(fileName,'r')
+
+df = pd.read_csv(f, sep=',', header=None, encoding='utf-8',
+                 names=['area', 'title', 'type', 'square', 'orient', 'place', 'floor', 'total_floor', 'price', 'year', 'unit_price'])
+
+
+print(df.describe())   #list the statistic table of all the numerical columns
