b4e2316dbb8abb668001dd30ec12e30269ef56e8
Liu Kaibo <liukaibo@Kaibo-MacBook-Pro.local>
2018-08-28 19:28:07 -0700

    add project of google translation spider

--- README.md
+++ README.md
@@ -25,3 +25,29 @@
 ### spider-rent
 - inspired from this [post](http://bigdata.51cto.com/art/201808/582085.htm), to crawl Beijing/Shanghai rent information from lianjia.com
 - use `requests` and `lxml` to extract information
+- `requests`：是用来请求对链家网进行访问的包。
+- `lxml`：解析网页，用 Xpath 表达式与正则表达式一起来获取网页信息，相比 bs4 速度更快。
+- beijing: https://bj.lianjia.com/zufang
+- shanghai: https://sh.lianjia.com/zufang
+- delimiter with `;`:
+	- awk: `awk -F; '{print $4, $9'`
+	- sort: `sort -rnt$',' -k9`
+		- `-k9` means sort against the 9th column
+		- `-n` means make the 9th column as numeric instead of string
+		- `-u` means merge all the duplicates in **9th column**
+		- `-r` means sort reversely
+- list the result with highest rent
+	```bash
+	cat -n rent_lianjia_bj_0.txt | sort -rnt$',' -k9|head -15| lolcat
+	```
+- color in awk
+	```bash
+	RED='\033[01;31m'
+	GREEN='\033[01;32m'
+	YELLOW='\033[01;33m'
+	BLUE='\033[01;34m'
+	NONE='\033[0m'
+	awk '{print $1, "\033[01;31m"$2"\033[0m", $3}'
+	```
+- 34min for Beijing
+- ?min for Shanghai
--- spider_house/spider_rent.py
+++ spider_house/spider_rent.py
@@ -5,7 +5,9 @@ from lxml import etree
 import pinyin
 import sys
 
- 
+city = 'sh'
+n_cut = 2 if city == 'bj' else 0
+base_url = 'https://{}.lianjia.com'.format(city) 
 # 获取某市区域的所有链接 
 def get_areas(url): 
     print('start grabing areas') 
@@ -17,11 +19,12 @@ def get_areas(url):
     areas = content.xpath("//dd[@data-index = '0']//div[@class='option-list']/a/text()") 
     print(areas)            # 08/27/2018 by kaibo
     areas_link = content.xpath("//dd[@data-index = '0']//div[@class='option-list']/a/@href") 
-    for i in range(1,len(areas)): 
+    for i in range(1,len(areas)-n_cut):      # kaibo: last of beijing 2 are '燕郊', '香河', which belongs to different domain (lf.lianjia.com instead of bj.lianjia.com 
+    #for i in range(13,len(areas)-2):     # kaibo: last 2 are '燕郊', '香河', which belongs to different domain (lf.lianjia.com instead of bj.lianjia.com 
         t1 = time.time()
         area = areas[i] 
         area_link = areas_link[i] 
-        link = 'https://bj.lianjia.com' + area_link 
+        link = base_url + area_link 
         print("开始抓取页面#{}-{}".format(i, area)) 
         get_pages(area, link) 
         print('{} area time: {:.5} s, total time: {:.5} s'.format('='*10, time.time()-t1, time.time()-t0))   # 08/27/2018 by kaibo
@@ -31,11 +34,12 @@ def get_pages(area,area_link):
     headers = { 
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'} 
     resposne = requests.get(area_link, headers=headers) 
-    pages =  int(re.findall("page-data=\'{\"totalPage\":(\d+),\"curPage\"", resposne.text)[0]) 
+    page_data = re.findall("page-data=\'{\"totalPage\":(\d+),\"curPage\"", resposne.text)    # 08/27/2018 by kaibo
+    pages =  0 if not page_data else int(page_data[0])        # 08/27/2018 by kaibo
     print("这个区域有" + str(pages) + "页") 
     for page in range(1,pages+1): 
         t11 = time.time()
-        url = 'https://bj.lianjia.com/zufang/{}/pg{}'.format(pinyin.get(area, format="strip"),str(page))     # 08/27/2018 by kaibo
+        url = '{}/zufang/{}/pg{}'.format(base_url, pinyin.get(area, format="strip"),str(page))     # 08/27/2018 by kaibo
         print("开始抓取[{}][page{}/{}]的信息".format(area, page, pages))    # 08/27/2018 by kaibo
         get_house_info(area,page,url) 
         print('{} page time: {:.5} s'.format('-'*10, time.time()-t11))  # 08/27/2018 by kaibo
@@ -67,9 +71,9 @@ def get_house_info(area, page, url):
             except Exception as e: 
                 house_year = "" 
             price = content.xpath("//div[@class='col-3']/div/span/text()")[i] 
-            with open('rent_lianjia_bj.txt','a',encoding='utf-8') as f: 
-                f.write(area + ',' + title + ',' + room_type + ',' + square + ',' +position+ 
-','+ detail_place+','+floor+','+total_floor+','+price+','+house_year+'\n') 
+            with open('rent_lianjia_{}.txt'.format(city),'a',encoding='utf-8') as f: 
+                #f.write(area + ',' + title + ',' + room_type + ',' + square + ',' +position+ ','+ detail_place+','+floor+','+total_floor+','+price+','+house_year+'\n') 
+                f.write('{},{},{},{},{},{},{},{},{},{}, {:.3}\n'.format(area, title, room_type, square,position, detail_place,floor,total_floor,price,house_year, int(price)/int(square)))
  
             sys.stdout.write('writing work [{}][page{}#{}/{}] has done!continue the next page {}'.format(area,page,i+1,len(titles),'\r'))    # 08/27/2018 by kaibo
             if i < len(titles)-1: sys.stdout.flush()
@@ -83,8 +87,7 @@ def get_house_info(area, page, url):
  
 def main(): 
     print('start!') 
-    url = 'https://bj.lianjia.com/zufang' 
-    get_areas(url) 
+    get_areas(base_url + '/zufang')
  
  
 if __name__ == '__main__': 
